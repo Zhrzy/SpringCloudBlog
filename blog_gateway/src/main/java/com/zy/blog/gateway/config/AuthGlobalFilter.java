a6188cac63518b142b1c7604c7e095e2678c350d
Zhrzy <1450290217@qq.com>
2022-09-02 10:23:32 +0800

    feature commit

--- blog_gateway/src/main/java/com/zy/blog/gateway/config/AuthGlobalFilter.java
+++ blog_gateway/src/main/java/com/zy/blog/gateway/config/AuthGlobalFilter.java
@@ -37,7 +37,7 @@ import java.util.Map;
 @Component
 public class AuthGlobalFilter implements GlobalFilter, Ordered {
 
-    /*测试add*/
+    /*测试add featureUpdate*/
     @Autowired
     private RedisTemplate redisTemplate;
     /*测试add*/
